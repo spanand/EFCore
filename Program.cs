92dfa2591b6d594d6c08c12767feb6c56f34c06a
Anandkumar Perumal <perumala@fcc.ca>
2020-01-25 00:20:46 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db= new BloggingContext())
+            {
+                Console.WriteLine("Adding a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                Console.WriteLine("Reading");
+                var blog = db.Blogs.OrderBy(a => a.BlogId).First();
+
+                Console.WriteLine("Updating");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(new Post
+                {
+                    Title="Hello worlds",
+                    Content="EF core practice"
+                });
+                db.SaveChanges();
+            }
+        }
+    }
+}
